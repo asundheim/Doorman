5f33dd6f8d6a0a7c55cc52acbc21866cb7e929c4
Anders Sundheim <andersundheim@gmail.com>
2019-02-11 17:13:50 -0600

    Refactored folders

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
-import './home.dart';
+import './pages/home.dart';
 void main() => runApp(Main());
 
 class Main extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Gatekeeper',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
